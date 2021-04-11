7755415d85695d49265f07f4e86e0cec42da3979
necocen <necocen@gmail.com>
2021-04-11 12:49:07 +0900

    add frontend

---
+++ frontend/snowpack.config.js
@@ -0,0 +1,27 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: "/src",
+    static: "/",
+  },
+  plugins: [
+    "@snowpack/plugin-typescript",
+    "@snowpack/plugin-react-refresh"
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  optimize: {
+    bundle: true,
+    minify: true
+  },
+};
