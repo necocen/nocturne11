c9dd0e3d45fe4aaf5142afc1ee34256c07beed88
necocen <necocen@gmail.com>
2021-04-19 23:14:28 +0900

    サイドメニューの伸縮

--- frontend/src/Months.tsx
+++ frontend/src/Months.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import dayjs from "dayjs";
 import useAxios from "axios-hooks";
 import { useRouting } from "./routing";
@@ -7,16 +7,18 @@ export function Months() {
     // 記事のある月の一覧を取得
     // デフォルトは2010年から現在まで（レイアウト崩れを防ぐためのものなので記事はない）
     const defaultYears = [...Array(dayjs().year() - 2010).keys()].map((y) => ({ year: y + 2010, months: undefined }));
+    const { thisMonth } = useRouting();
     const [{ data: { years } = { years: defaultYears } }] = useAxios<{ years: { year: number; months?: number[] }[] }>({
         url: "http://localhost:4000/api/months",
     });
+    const [expandedYear, setExpandedYear] = useState(thisMonth.year());
 
     return (
         <>
             {years
                 .sort((y1, y2) => y1.year - y2.year)
                 .map((year) => (
-                    <Year key={year.year} {...year} />
+                    <Year key={year.year} {...year} expand={setExpandedYear} expanded={expandedYear == year.year} />
                 ))}
         </>
     );
@@ -25,9 +27,11 @@ export function Months() {
 type YearProps = {
     year: number;
     months?: number[];
+    expanded: boolean;
+    expand: (year: number) => void;
 };
 
-function Year({ year, months }: YearProps) {
+function Year({ year, months, expand, expanded }: YearProps) {
     const { dayjsToPath } = useRouting();
     const monthRows = [
         [1, 2, 3, 4, 5, 6],
@@ -35,8 +39,8 @@ function Year({ year, months }: YearProps) {
     ].map((row) => row.map((m) => dayjs(`${year}-${m.toString().padStart(2, "0")}-01`)));
     return (
         <table className="year-month">
-            <caption>{year}</caption>
-            <tbody>
+            <caption onClick={() => expand(year)}>{year}</caption>
+            <tbody className={expanded ? "expanded" : undefined}>
                 {monthRows.map((monthRow, index) => (
                     <tr key={index}>
                         {monthRow.map((month) => (
