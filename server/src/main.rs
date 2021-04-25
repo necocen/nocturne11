2b6daaa6047c704fbc31a2c200c5ccd3ccb19648
necocen <necocen@gmail.com>
2021-04-25 17:23:21 +0900

    セッション管理機構の導入

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 use actix_cors::Cors;
-use actix_web::App;
+use actix_identity::{CookieIdentityPolicy, IdentityService};
+use actix_web::{cookie::SameSite, App};
 use anyhow::Result;
 mod server;
 use server::Server;
@@ -12,12 +13,21 @@ use std::env;
 async fn main() -> Result<()> {
     env_logger::init();
     dotenv().ok();
+    let secret_key = env::var("SECRET_KEY")?;
+    if secret_key.len() < 32 {
+        panic!("SECRET_KEY is not long enough.");
+    }
     let es_url = url::Url::parse(&env::var("ES_URL")?)?;
     let pg_url = url::Url::parse(&env::var("DATABASE_URL")?)?;
     let server = Server::new(&es_url, &pg_url)?;
     actix_web::HttpServer::new(move || {
         let cors = Cors::default().allowed_origin("http://localhost:8080"); // for development
+        let identity_policy = CookieIdentityPolicy::new(secret_key.as_bytes())
+            .name("nocturne-session")
+            .same_site(SameSite::Lax)
+            .secure(false); // for development
         App::new()
+            .wrap(IdentityService::new(identity_policy))
             .wrap(cors)
             .configure(routing(server.clone(), "./frontend/build/src"))
     })
