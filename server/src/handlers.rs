35e92197e780b1e60744c6ce5e6197dc1a922e2f
necocen <necocen@gmail.com>
2021-04-26 22:54:16 +0900

    refactor

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -1,7 +1,7 @@
 use crate::server::Server;
 use actix_cors::Cors;
 use actix_files as fs;
-use actix_web::web;
+use actix_web::web::{get, post, resource, scope, ServiceConfig};
 use errors::Error;
 use std::path::PathBuf;
 use templates::TemplateToResponse;
@@ -15,34 +15,33 @@ mod templates;
 pub(crate) fn routing(
     server: Server,
     static_path: impl Into<PathBuf>,
-) -> Box<dyn FnOnce(&mut web::ServiceConfig)> {
+) -> Box<dyn FnOnce(&mut ServiceConfig)> {
     let static_path: PathBuf = static_path.into();
-    Box::new(move |cfg: &mut web::ServiceConfig| {
+    Box::new(move |cfg: &mut ServiceConfig| {
         let cors = Cors::default().allowed_origin("http://localhost:8080"); // for development
         cfg.data(server.clone())
-            .service(web::resource("/").route(web::get().to(posts::all_posts)))
+            .service(resource("/").route(get().to(posts::all_posts)))
             .service(
-                web::scope("/admin")
-                    .service(web::resource("/new").route(web::get().to(admin::new_post_form)))
-                    .service(web::resource("/create").route(web::post().to(admin::create))),
+                scope("/admin")
+                    .service(resource("/new").route(get().to(admin::new_post_form)))
+                    .service(resource("/create").route(post().to(admin::create))),
             )
-            .service(web::resource(r"/{id:\d+}").route(web::get().to(posts::post_with_id)))
+            .service(resource(r"/{id:\d+}").route(get().to(posts::post_with_id)))
             .service(
-                web::resource(r"/{year:\d{4}}-{month:\d{2}}")
-                    .route(web::get().to(posts::posts_with_date)),
-            )
-            .service(
-                web::resource(r"/{year:\d{4}}-{month:\d{2}}-{day:\d{2}}")
-                    .route(web::get().to(posts::posts_with_date)),
+                resource(r"/{year:\d{4}}-{month:\d{2}}").route(get().to(posts::posts_with_date)),
             )
             .service(
-                web::scope("/api")
+                resource(r"/{year:\d{4}}-{month:\d{2}}-{day:\d{2}}")
+                    .route(get().to(posts::posts_with_date)),
+            )
+            .service(
+                scope("/api")
                     .wrap(cors)
                     .service(
-                        web::resource(r"/days/{year:\d{4}}-{month:\d{2}}")
-                            .route(web::get().to(api::days_in_year_month)),
+                        resource(r"/days/{year:\d{4}}-{month:\d{2}}")
+                            .route(get().to(api::days_in_year_month)),
                     )
-                    .service(web::resource("/months").route(web::get().to(api::months))),
+                    .service(resource("/months").route(get().to(api::months))),
             )
             .service(fs::Files::new("/static", static_path));
     })
