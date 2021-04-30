d7ffd0546e47b825180a84c5c44d407b10585e42
necocen <necocen@gmail.com>
2021-04-30 23:05:46 +0900

    インポート時にElasticSearchにindexする

--- infrastructure/src/bin/import.rs
+++ infrastructure/src/bin/import.rs
@@ -4,32 +4,38 @@ extern crate log;
 use anyhow::Result;
 use domain::{
     entities::Post,
-    repositories::{export_posts::ExportPostsRepository, import_posts::ImportPostsRepository},
+    repositories::{
+        export_posts::ExportPostsRepository, import_posts::ImportPostsRepository,
+        search::SearchRepository,
+    },
 };
 use dotenv::dotenv;
 use infrastructure::{
     legacy::posts_repository_impl::OldPostsRepositoryImpl,
-    posts_repository_impl::PostsRepositoryImpl,
+    posts_repository_impl::PostsRepositoryImpl, search_repository_impl::SearchRepositoryImpl,
 };
 use std::env;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     env_logger::init();
     dotenv().ok();
     let old_repo = OldPostsRepositoryImpl::new(&url::Url::parse(&env::var("OLD_DATABASE_URL")?)?)?;
     let new_repo = PostsRepositoryImpl::new(&url::Url::parse(&env::var("DATABASE_URL")?)?)?;
-    transport(&old_repo, &new_repo)?;
+    let search_repo = SearchRepositoryImpl::new(&url::Url::parse(&env::var("ES_URL")?)?)?;
+    transport(&old_repo, &new_repo, &search_repo).await?;
     Ok(())
 }
 
-fn transport(
-    old_repository: &impl ExportPostsRepository,
-    new_repository: &impl ImportPostsRepository,
+async fn transport(
+    old_repo: &impl ExportPostsRepository,
+    new_repo: &impl ImportPostsRepository,
+    search_repo: &impl SearchRepository,
 ) -> Result<()> {
     let page_size = 100_usize;
     let mut old_posts: Vec<Post> = vec![];
     loop {
-        let mut chunk = old_repository.get_all(old_posts.len(), page_size)?;
+        let mut chunk = old_repo.get_all(old_posts.len(), page_size)?;
         let count = chunk.len();
         if count == 0 {
             break;
@@ -50,9 +56,11 @@ fn transport(
         old_posts.append(&mut chunk);
     }
 
-    let imported = new_repository.import(old_posts)?;
-    new_repository.reset_id_sequence()?;
+    let imported = new_repo.import(&old_posts)?;
     info!("Imported {} posts successfully.", imported.len());
+    search_repo.insert_bulk(&old_posts).await?;
+    info!("Indexed {} posts successfully.", imported.len());
+    new_repo.reset_id_sequence()?;
 
     Ok(())
 }
