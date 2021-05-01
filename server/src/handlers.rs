acff004c4be445148f62814be922a0809200bec1
necocen <necocen@gmail.com>
2021-05-01 18:23:21 +0900

    編集画面仮実装

--- server/src/handlers.rs
+++ server/src/handlers.rs
@@ -33,6 +33,7 @@ pub(crate) fn routing(server: Server) -> Box<dyn FnOnce(&mut ServiceConfig)> {
                 scope("/admin")
                     .wrap(AuthService::new(move |id| id == admin_user_id))
                     .service(resource("/new").route(get().to(admin::new_post_form)))
+                    .service(resource("/edit").route(get().to(admin::edit_post_form)))
                     .service(resource("/create").route(post().to(admin::create))),
             )
             .service(
--- server/src/handlers/admin.rs
+++ server/src/handlers/admin.rs
@@ -1,11 +1,10 @@
-use super::Error;
-use super::TemplateToResponse;
+use super::{args::IdArguments, Error, TemplateToResponse};
 use crate::server::Server;
 use actix_web::{http::header, web, HttpResponse};
 use chrono::Utc;
-use domain::{entities::NewPost, use_cases::create_post};
+use domain::{entities::NewPost, repositories::posts::PostsRepository, use_cases::create_post};
 use serde::Deserialize;
-use templates::NewPostTemplate;
+use templates::{EditPostTemplate, NewPostTemplate};
 
 #[derive(Debug, Clone, Deserialize)]
 pub(super) struct FormParams {
@@ -15,6 +14,18 @@ pub(super) struct FormParams {
 
 pub(super) async fn new_post_form(_server: web::Data<Server>) -> Result<HttpResponse, Error> {
     NewPostTemplate { title: "投稿" }.to_response()
+}
+
+pub(super) async fn edit_post_form(
+    server: web::Data<Server>,
+    args: web::Query<IdArguments>,
+) -> Result<HttpResponse, Error> {
+    let post = &server.posts_repository.get(args.id)?;
+    EditPostTemplate {
+        title: "編集",
+        post,
+    }
+    .to_response()
 }
 
 pub(super) async fn create(
@@ -38,9 +49,18 @@ pub(super) async fn create(
 }
 
 mod templates {
+    use domain::entities::Post;
+
     #[derive(askama::Template)]
     #[template(path = "admin/new.html")]
     pub(super) struct NewPostTemplate<'a> {
         pub(super) title: &'a str,
+    }
+
+    #[derive(askama::Template)]
+    #[template(path = "admin/edit.html")]
+    pub(super) struct EditPostTemplate<'a> {
+        pub(super) title: &'a str,
+        pub(super) post: &'a Post,
     }
 }
