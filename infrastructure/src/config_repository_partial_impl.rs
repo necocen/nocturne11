27aff77179b58ed57cc40b55f7d7ac8655d47f9e
necocen <necocen@gmail.com>
2021-05-07 13:56:12 +0900

    ConfigRepositoryを分割

--- infrastructure/src/config_repository_partial_impl.rs
+++ infrastructure/src/config_repository_partial_impl.rs
@@ -2,7 +2,7 @@ use crate::models::Config as ConfigModel;
 use anyhow::Result;
 use diesel::prelude::*;
 use diesel::{r2d2::ConnectionManager, PgConnection, RunQueryDsl};
-use domain::{entities::config::Config, repositories::config::ConfigRepository};
+use domain::entities::config::Link;
 use r2d2::Pool;
 use std::sync::Arc;
 
@@ -11,8 +11,14 @@ pub struct ConfigRepositoryPartialImpl {
     pub(crate) conn_pool: Arc<Pool<ConnectionManager<PgConnection>>>,
 }
 
-impl ConfigRepository for ConfigRepositoryPartialImpl {
-    fn get(&self) -> Result<Config> {
+pub struct PartialConfig {
+    pub about: String,
+    pub mathjax_options: String,
+    pub links: Vec<Link>,
+}
+
+impl ConfigRepositoryPartialImpl {
+    pub fn get_partial(&self) -> Result<PartialConfig> {
         use crate::schema::configs::dsl::configs;
         let records = configs.get_results::<ConfigModel>(&self.conn_pool.get()?)?;
         let about = records
@@ -23,19 +29,14 @@ impl ConfigRepository for ConfigRepositoryPartialImpl {
             .iter()
             .find(|c| c.key == "mathjax_options")
             .map_or(String::new(), |c| c.value.to_owned());
-        Ok(Config {
-            title: "andante".to_owned(),
-            description: "個人的な日記".to_owned(),
-            author: "κねこせん".to_owned(),
-            email: "necocen@gmail.com".to_owned(),
-            generator: "Nocturne v11".to_owned(),
+        Ok(PartialConfig {
             about,
             mathjax_options,
             links: vec![],
         })
     }
 
-    fn set_about(&self, about: &str) -> Result<()> {
+    pub fn set_about(&self, about: &str) -> Result<()> {
         use crate::schema::configs::{self, key, value};
         diesel::insert_into(configs::table)
             .values((key.eq("about"), value.eq(about)))
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
+use domain::{entities::config::Config, repositories::config::ConfigRepository};
 use infrastructure::{
-    config_repository_partial_impl::ConfigRepositoryPartialImpl,
-    pg_repository_impls::pg_repositories, posts_repository_impl::PostsRepositoryImpl,
+    config_repository_partial_impl::{ConfigRepositoryPartialImpl, PartialConfig},
+    pg_repository_impls::pg_repositories,
+    posts_repository_impl::PostsRepositoryImpl,
     search_repository_impl::SearchRepositoryImpl,
 };
 
@@ -9,7 +11,7 @@ use infrastructure::{
 pub struct Server {
     pub search_repository: SearchRepositoryImpl,
     pub posts_repository: PostsRepositoryImpl,
-    pub config_repository: ConfigRepositoryPartialImpl,
+    pub config_repository: ConfigRepositoryImpl,
     pub admin_user_id: String,
 }
 
@@ -20,16 +22,48 @@ impl Server {
         admin_user: impl Into<String>,
     ) -> Result<Self> {
         let search_repository = SearchRepositoryImpl::new(es_url)?;
-        let (posts_repository, config_repository) = pg_repositories(pg_url)?;
+        let (posts_repository, config_repository_partial) = pg_repositories(pg_url)?;
         Ok(Server {
             search_repository,
             posts_repository,
-            config_repository,
+            config_repository: ConfigRepositoryImpl {
+                partial: config_repository_partial,
+            },
             admin_user_id: admin_user.into(),
         })
     }
 
     pub fn authorize(&self, id: &str) -> bool {
         id == self.admin_user_id
+    }
+}
+
+#[derive(Clone)]
+pub struct ConfigRepositoryImpl {
+    partial: ConfigRepositoryPartialImpl,
+}
+
+impl ConfigRepository for ConfigRepositoryImpl {
+    fn get(&self) -> Result<Config> {
+        let PartialConfig {
+            about,
+            mathjax_options,
+            links,
+        } = self.partial.get_partial()?;
+
+        Ok(Config {
+            title: "andante".to_owned(),
+            description: "個人的な日記".to_owned(),
+            author: "κねこせん".to_owned(),
+            email: "necocen@gmail.com".to_owned(),
+            generator: format!("Nocturne v{} {}", env!("VERGEN_BUILD_SEMVER"), env!("VERGEN_BUILD_TIMESTAMP")),
+            about,
+            mathjax_options,
+            links,
+        })
+    }
+
+    fn set_about(&self, about: &str) -> Result<()> {
+        self.partial.set_about(about)
     }
 }
