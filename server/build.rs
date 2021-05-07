3b4e419c0cde15620ebbe81deaf71b9a36f26195
necocen <necocen@gmail.com>
2021-05-07 13:55:44 +0900

    ビルド時に埋め込まれるタイムスタンプをローカル時刻に

--- server/build.rs
+++ server/build.rs
@@ -1,6 +1,8 @@
-use vergen::{Config, vergen};
+use vergen::{vergen, Config, TimeZone};
 
 fn main() {
-  // Generate the default 'cargo:' instruction output
-  vergen(Config::default()).ok();
+    // Generate the default 'cargo:' instruction output
+    let mut config = Config::default();
+    *config.build_mut().timezone_mut() = TimeZone::Local;
+    vergen(config).ok();
 }
