f3f2e41b39b823a561111cb880654080ecc874f1
necocen <necocen@gmail.com>
2021-05-08 18:20:35 +0900

    約物検出改善とスタイル調整
    ただし約物検出はバックエンドに移す予定

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,12 @@ import { Nav } from "./Nav";
 
 ReactDOM.render(<Nav />, document.getElementById("side-nav"));
 
+function appendClassName(span: HTMLSpanElement, className: string) {
+    const classNames = span.className.split(" ").filter((c) => c.length > 0);
+    classNames.push(className);
+    span.className = classNames.join(" ");
+}
+
 function modifyYakumonos() {
     for (const paragraph of document.querySelectorAll("article p")) {
         const modified = [...paragraph.childNodes].flatMap((node) => {
@@ -11,8 +17,29 @@ function modifyYakumonos() {
                 return [...(node.textContent ?? "")]
                     .map((c) => {
                         // 約物を検出して<span>で囲みます
-                        if (c == "「") {
+                        if ("「『（〈【｛［《〔“".includes(c)) {
                             const span = document.createElement("span");
+                            appendClassName(span, "yakumono-open-bracket");
+                            span.appendChild(document.createTextNode(c));
+                            return span;
+                        } else if ("」』）〉】｝］》〕”".includes(c)) {
+                            const span = document.createElement("span");
+                            appendClassName(span, "yakumono-close-bracket");
+                            span.appendChild(document.createTextNode(c));
+                            return span;
+                        } else if ("、。，．".includes(c)) {
+                            const span = document.createElement("span");
+                            appendClassName(span, "yakumono-punctuation");
+                            span.appendChild(document.createTextNode(c));
+                            return span;
+                        } else if ("・".includes(c)) {
+                            const span = document.createElement("span");
+                            appendClassName(span, "yakumono-interpunct");
+                            span.appendChild(document.createTextNode(c));
+                            return span;
+                        } else if ("／＼！？".includes(c)) {
+                            const span = document.createElement("span");
+                            appendClassName(span, "yakumono-other");
                             span.appendChild(document.createTextNode(c));
                             return span;
                         } else {
@@ -24,15 +51,25 @@ function modifyYakumonos() {
                         if (acc.length == 0) {
                             return [r];
                         }
-                        if (acc[acc.length - 1].nodeType == node.TEXT_NODE && r.nodeType == node.TEXT_NODE) {
-                            const last = acc.pop()!;
+                        const last = acc.pop()!;
+                        if (last.nodeType == node.TEXT_NODE && r.nodeType == node.TEXT_NODE) {
                             last.textContent = (last.textContent ?? "") + (r.textContent ?? "");
                             acc.push(last);
                         } else {
+                            acc.push(last);
                             acc.push(r);
                         }
                         return acc;
-                    }, [] as (HTMLSpanElement | Text)[]);
+                    }, [] as (HTMLSpanElement | Text)[]).map((node) => {
+                        if (node.nodeType == node.TEXT_NODE) {
+                            const span = document.createElement("span");
+                            span.appendChild(document.createTextNode(node.textContent ?? ""));
+                            appendClassName(span, "non-yakumono");
+                            return span;
+                        } else {
+                            return node;
+                        }
+                    })
             } else {
                 return [node];
             }
@@ -42,5 +79,4 @@ function modifyYakumonos() {
         modified.forEach((node) => paragraph.append(node));
     }
 }
-
 modifyYakumonos();
