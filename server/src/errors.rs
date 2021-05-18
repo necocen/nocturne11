b9cc61f9de5e7ef2884a119abaed17411048c24d
necocen <necocen@gmail.com>
2021-05-19 08:45:14 +0900

    refactor

--- server/src/errors.rs
+++ server/src/errors.rs
@@ -1,8 +1,3 @@
-use actix_web::{
-    dev::HttpResponseBuilder,
-    http::{header, StatusCode},
-    HttpResponse, ResponseError,
-};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -13,23 +8,4 @@ pub enum Error {
     Askama(#[from] askama::Error),
     #[error(transparent)]
     Other(#[from] anyhow::Error),
-}
-
-impl ResponseError for Error {
-    fn status_code(&self) -> StatusCode {
-        use domain::repositories::posts::Error::NotFound;
-        use domain::Error::Posts;
-        if let Self::Domain(Posts(NotFound(_))) = self {
-            StatusCode::NOT_FOUND
-        } else {
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
-
-    fn error_response(&self) -> HttpResponse {
-        HttpResponseBuilder::new(self.status_code())
-            .insert_header((header::CONTENT_TYPE, "text/html; charset=utf-8"))
-            .body(self.to_string())
-        // TODO: log
-    }
 }
--- server/src/handlers/errors.rs
+++ server/src/handlers/errors.rs
@@ -1,14 +1,23 @@
 use self::templates::{InternalErrorTemplate, NotFoundTemplate, UnauthorizedTemplate};
-use crate::{askama_helpers::TemplateToResponse, context::AppContext};
-use actix_web::{Result, dev::ServiceResponse, http::StatusCode, middleware::ErrorHandlerResponse};
+use crate::{askama_helpers::TemplateToResponse, context::AppContext, errors::Error};
+use actix_web::{
+    dev::{HttpResponseBuilder, ServiceResponse},
+    http::{header, StatusCode},
+    middleware::ErrorHandlerResponse,
+    HttpResponse, ResponseError, Result,
+};
 use futures::{future::FutureExt, TryStreamExt};
 
 pub fn error_401(res: ServiceResponse) -> Result<ErrorHandlerResponse<actix_web::dev::Body>> {
-    let req = res.request();
     // AppContextが取得できればそれを使ってテンプレートを描画する
     // 取得できない場合は何もしない
-    if let Some(context) = req.clone().extensions().get::<AppContext>() {
-        let context = context.clone();
+    if let Some(context) = res
+        .request()
+        .clone()
+        .extensions()
+        .get::<AppContext>()
+        .cloned()
+    {
         let body = UnauthorizedTemplate { context }.to_response()?.take_body();
         Ok(ErrorHandlerResponse::Response(res.map_body(|_, _| body)))
     } else {
@@ -17,11 +26,15 @@ pub fn error_401(res: ServiceResponse) -> Result<ErrorHandlerResponse<actix_web:
 }
 
 pub fn error_404(res: ServiceResponse) -> Result<ErrorHandlerResponse<actix_web::dev::Body>> {
-    let req = res.request();
     // AppContextが取得できればそれを使ってテンプレートを描画する
     // 取得できない場合は何もしない
-    if let Some(context) = req.clone().extensions().get::<AppContext>() {
-        let context = context.clone();
+    if let Some(context) = res
+        .request()
+        .clone()
+        .extensions()
+        .get::<AppContext>()
+        .cloned()
+    {
         let body = NotFoundTemplate { context }.to_response()?.take_body();
         Ok(ErrorHandlerResponse::Response(res.map_body(|_, _| body)))
     } else {
@@ -32,20 +45,43 @@ pub fn error_404(res: ServiceResponse) -> Result<ErrorHandlerResponse<actix_web:
 pub fn error_500(mut res: ServiceResponse) -> Result<ErrorHandlerResponse<actix_web::dev::Body>> {
     // AppContextが取得できればそれを使ってテンプレートを描画する
     // 取得できない場合は何もしない
-    if let Some(context) = res.request().clone().extensions().get::<AppContext>() {
-        let context = context.clone();
+    if let Some(context) = res
+        .request()
+        .clone()
+        .extensions()
+        .get::<AppContext>()
+        .cloned()
+    {
         Ok(ErrorHandlerResponse::Future(Box::pin(
             res.take_body().try_collect::<Vec<_>>().map(move |result| {
                 let req = res.request().clone();
-                let error = std::str::from_utf8(&result?.concat())?.to_owned();
-                let mut res = InternalErrorTemplate { context, error }.to_response()?;
-                let status = res.status_mut();
-                *status = StatusCode::INTERNAL_SERVER_ERROR;
+                let status = res.status();
+                let body = std::str::from_utf8(&result?.concat())?.to_owned();
+                let mut res = InternalErrorTemplate { context, body }.to_response()?;
+                *res.status_mut() = status;
                 Ok(ServiceResponse::new(req, res))
             }),
         )))
     } else {
         Ok(ErrorHandlerResponse::Response(res))
+    }
+}
+
+impl ResponseError for Error {
+    fn status_code(&self) -> StatusCode {
+        use domain::repositories::posts::Error::NotFound;
+        use domain::Error::Posts;
+        if let Self::Domain(Posts(NotFound(_))) = self {
+            StatusCode::NOT_FOUND
+        } else {
+            StatusCode::INTERNAL_SERVER_ERROR
+        }
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        HttpResponseBuilder::new(self.status_code())
+            .insert_header((header::CONTENT_TYPE, "text/html; charset=utf-8"))
+            .body(self.to_string())
     }
 }
 
@@ -69,6 +105,6 @@ mod templates {
     #[template(path = "errors/internal_error.html")]
     pub struct InternalErrorTemplate {
         pub context: AppContext,
-        pub error: String,
+        pub body: String,
     }
 }
