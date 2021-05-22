0cadc91c90a0997ae7df15aca9707049fa4bc0e3
necocen <necocen@gmail.com>
2021-05-22 20:16:57 +0900

    migrateの修正

--- infrastructure/src/bin/migrate.rs
+++ infrastructure/src/bin/migrate.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use dotenv::dotenv;
 use infrastructure::migration::migrate;
 use std::env;
@@ -6,6 +6,7 @@ use std::env;
 fn main() -> Result<()> {
     env_logger::init();
     dotenv().ok();
-    let pg_url = url::Url::parse(&env::var("DATABASE_NAME")?)?;
+    let pg_url =
+        url::Url::parse(&env::var("DATABASE_URL").context("DATABASE_URL was not found")?)?;
     migrate(&pg_url)
 }
