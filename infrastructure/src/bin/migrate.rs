4d8df201d8d0acfa81f1d81ce5ed1bd4991b572d
necocen <necocen@gmail.com>
2021-05-22 21:56:35 +0900

    cargo fmt

--- infrastructure/src/bin/migrate.rs
+++ infrastructure/src/bin/migrate.rs
@@ -6,7 +6,6 @@ use std::env;
 fn main() -> Result<()> {
     env_logger::init();
     dotenv().ok();
-    let pg_url =
-        url::Url::parse(&env::var("DATABASE_URL").context("DATABASE_URL was not found")?)?;
+    let pg_url = url::Url::parse(&env::var("DATABASE_URL").context("DATABASE_URL was not found")?)?;
     migrate(&pg_url)
 }
