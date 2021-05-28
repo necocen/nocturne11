b7098d58f88ef0a1dedb0fd24f46ea4aa7904b39
necocen <necocen@gmail.com>
2021-05-28 20:50:23 +0900

    投稿フォームのReact化

---
+++ frontend/ts/Form.tsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from "react";
+
+type Props = {
+    title?: string;
+    body?: string;
+    id?: string;
+};
+
+export function Form(props: Props) {
+    const formRef = useRef<HTMLFormElement>(null);
+    const deleteFormRef = useRef<HTMLFormElement>(null);
+    const [title, setTitle] = useState(props.title ?? "");
+    const [body, setBody] = useState(props.body ?? "");
+
+    const submit = () => formRef.current?.submit();
+    const submitDelete = () => deleteFormRef.current?.submit();
+
+    return (
+        <>
+            <form id={props.id ? "form-update" : "form-create"} method="POST" action={props.id ? "/admin/update" : "/admin/create"} ref={formRef}>
+                <fieldset>
+                    <h4>
+                        <label htmlFor="post-form-title">タイトル</label>
+                    </h4>
+                    <p>
+                        <input type="text" name="title" id="post-form-title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        <input type="text" id="dummy-to-prevent-submit" style={{ display: "none" }} />
+                    </p>
+                    <h4>
+                        <label htmlFor="post-form-body">本文</label>
+                    </h4>
+                    <p>
+                        <textarea name="body" id="post-form-body" value={body} onChange={(e) => setBody(e.target.value)} />
+                    </p>
+                </fieldset>
+                <p>
+                    {props.id && <input type="hidden" name="id" id="post-form-id" value={props.id} />}
+                    <button type="button" onClick={submit}>
+                        送信
+                    </button>
+                </p>
+            </form>
+            {props.id && (
+                <form id="form-delete" method="POST" action="/admin/delete" ref={deleteFormRef}>
+                    <p>
+                        <input type="hidden" name="id" id="post-form-id" value={props.id} />
+                        <button type="button" onClick={submitDelete}>
+                            削除
+                        </button>
+                    </p>
+                </form>
+            )}
+        </>
+    );
+}
--- frontend/ts/index.tsx
+++ frontend/ts/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Nav } from "./Nav";
 import { LoginButton, LogoutButton } from "./Login";
+import { Form } from "./Form";
 
 ReactDOM.render(<Nav />, document.getElementById("side-nav"));
 const button = document.getElementById("google-login");
@@ -12,6 +13,11 @@ if (button) {
 const logout = document.getElementsByClassName("logout-button");
 for (const el of logout) {
     ReactDOM.render(<LogoutButton />, el);
+}
+
+const diaryForm = document.getElementById("diary-form-slot");
+if (diaryForm) {
+    ReactDOM.render(<Form id={diaryForm.dataset.id} title={diaryForm.dataset.title} body={diaryForm.dataset.body} />, diaryForm);
 }
 
 // rel="external"のついたリンクは別タブで開く
