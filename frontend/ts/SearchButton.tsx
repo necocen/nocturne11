a8e4e13c53f707cb90cd0362fbba04b3c30424e7
necocen <necocen@gmail.com>
2021-05-29 10:30:36 +0900

    CSS: 検索ボックス

--- frontend/ts/SearchButton.tsx
+++ frontend/ts/SearchButton.tsx
@@ -7,8 +7,20 @@ export function SearchButton() {
             return;
         }
         if (searchForm.classList.contains("search-box-hidden")) {
+            for (const input of searchForm.getElementsByTagName("input")) {
+                input.disabled = false;
+            }
+            for (const button of searchForm.getElementsByTagName("button")) {
+                button.disabled = false;
+            }
             searchForm.classList.remove("search-box-hidden");
         } else {
+            for (const input of searchForm.getElementsByTagName("input")) {
+                input.disabled = true;
+            }
+            for (const button of searchForm.getElementsByTagName("button")) {
+                button.disabled = true;
+            }
             searchForm.classList.add("search-box-hidden");
         }
     }, [searchForm]);
